71405b24041edf84c9a24d5b57546bf7fefb2e91
MohanedMohamedAhmed <132159595+MohanedMohamedAhmed@users.noreply.github.com>
2023-05-01 21:11:15 +0200

    create widget

--- lib/views/widges/notes_view_body.dart
+++ lib/views/widges/notes_view_body.dart
@@ -2,14 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class Notesview extends StatelessWidget {
-  const Notesview({super.key});
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold( 
-      body: NotesViewBody(),
-    );
+    return Container();
   }
 }
