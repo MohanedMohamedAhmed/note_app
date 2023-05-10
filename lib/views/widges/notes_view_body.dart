0e9ce3568731b6f7eaf4e4ed599ffbfe7badfbf9
MohanedMohamedAhmed <132159595+MohanedMohamedAhmed@users.noreply.github.com>
2023-05-10 09:15:36 +0200

    create custom note items

--- lib/views/widges/notes_view_body.dart
+++ lib/views/widges/notes_view_body.dart
@@ -20,47 +20,6 @@ class NotesViewBody extends StatelessWidget {
     );
   }
 }
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-  
-  get trailing => null;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color:const Color(0xffFFCCBD),
-        borderRadius:BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title:const Text('flutter tips' ,
-            style:
-             TextStyle(
-              color:colors.black,
-              fontSize: 26,
-            ),
-            ),
-            subtitle:Text('Build your career with mohamed mohamed' ,style:    TextStyle(
-              color:colors.black ,withOpacity(0.4),
-              fontSize: 20,
-            ),
-            ),
-            ),
-            trailing: IconBotton(onPressed: () {},icon: Icon(Icons.delete,color:colors.black,size:30,),
-          ),
-          Text('may21 ,2022',
-          color:Colors.black,)
-        ],
-          ),
-        
-      );
-    
-  }
-}
-
 
 
 
