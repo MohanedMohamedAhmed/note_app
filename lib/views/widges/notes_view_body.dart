1f0c7f4cd23626413300f5fd5bf7fa118e868d58
MohanedMohamedAhmed <132159595+MohanedMohamedAhmed@users.noreply.github.com>
2023-05-11 17:28:37 +0200

    create notes list view

--- lib/views/widges/notes_view_body.dart
+++ lib/views/widges/notes_view_body.dart
@@ -1,11 +1,15 @@
-import 'package:flutter/material.dart' show BorderRadius, BoxDecoration, BuildContext, Color, Colors, Column, Container, CrossAxisAlignment, EdgeInsets, Icon, Icons, ListTile, Padding, SizedBox, StatelessWidget, Text, TextStyle, Widget;
+import 'package:flutter/material.dart' show BorderRadius, BoxDecoration, BuildContext, Color, Colors, Column, Container, CrossAxisAlignment, EdgeInsets, Expanded, Icon, Icons, ListTile, ListView, Padding, SizedBox, StatelessWidget, Text, TextStyle, Widget;
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_application_1/views/widges/custom_note_item.dart';
 
 import 'custom_app_bar.dart';
+import 'notes_listview.dart';
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
+    
     return Padding(
       padding:const EdgeInsets.symmetric(horizontal:24),
      child:Column(
@@ -14,7 +18,8 @@ class NotesViewBody extends StatelessWidget {
           height:50,
         ),
         CustomAppBar(),
-        NoteItem(),
+        Expanded(child: NotesListView()),
+        
       ],
     ),
     );
