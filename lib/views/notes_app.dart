f7efa0e11cdda16930667d536d6baa89b84aaad9
MohanedMohamedAhmed <132159595+MohanedMohamedAhmed@users.noreply.github.com>
2023-05-11 17:44:12 +0200

    add floating action button

--- lib/views/notes_app.dart
+++ lib/views/notes_app.dart
@@ -9,8 +9,11 @@ class Notesview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return  const Scaffold( 
-      body: NotesViewBody(),
+    return  Scaffold( 
+      floatingActionButton: FloatingActionButton(onPressed: () {},child:const Icon(Icons.add),
+      ),
+
+      body:const NotesViewBody(),
     );
   }
 }
