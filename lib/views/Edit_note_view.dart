a6b9b3cf3d8b380b1e41019059664d0d4044ed80
MohanedMohamedAhmed <132159595+MohanedMohamedAhmed@users.noreply.github.com>
2023-05-13 21:41:57 +0200

    create edit note view

---
+++ lib/views/Edit_note_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_application_1/views/widges/Edit_view_body.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: EditNoteViewBody(),
+    );
+  }
+}
--- lib/views/widges/custom_note_item.dart
+++ lib/views/widges/custom_note_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import '../Edit_note_view.dart';
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
   
@@ -6,45 +8,57 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24,bottom: 24,left: 16),
-      decoration: BoxDecoration(
-        color:const Color(0xffFFCCBD),
-        borderRadius:BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            
-            title:const Text('flutter tips' ,
-            style:
-             TextStyle(
-              color:colors.black,
-              fontSize: 26,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context){
+          return const EditNoteView();
+        }
+         ),
+        );
+      },
+    
+      child: Container(
+        padding: const EdgeInsets.only(top: 24,bottom: 24,left: 16),
+        decoration: BoxDecoration(
+          color:const Color(0xffFFCCBD),
+          borderRadius:BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              
+              title:const Text('flutter tips' ,
+              style:
+               TextStyle(
+                color:colors.black,
+                fontSize: 26,
+              ),
+              ),
+              subtitle:Text('Build your career with mohamed mohamed' ,style:    TextStyle(
+                color:Colors.black ,withOpacity(0.5),
+                fontSize: 18,
+              ),
+              ),
+              ),
+              trailing: IconBotton(onPressed: () {},icon:const Icon(Icons.delete,color:colors.black,size:24,),
+            ),
+            Padding(
+              Padding:const EdgeInsets.only( right:24)
+    
+              child:   Text('may21 ,2022',
+              style: TextStyle(
+            color:colors.black ,withOpacity(0.4),
+              ),
             ),
             ),
-            subtitle:Text('Build your career with mohamed mohamed' ,style:    TextStyle(
-              color:colors.black ,withOpacity(0.5),
-              fontSize: 18,
+          ],
             ),
-            ),
-            ),
-            trailing: IconBotton(onPressed: () {},icon:const Icon(Icons.delete,color:colors.black,size:24,),
-          ),
-          Padding(
-            Padding:const EdgeInsets.only( right:24)
-
-            child:   Text('may21 ,2022',
-            style: TextStyle(
-          color:colors.black ,withOpacity(0.4),
-            ),
-          ),
-          ),
-        ],
-          ),
-        
-      );
+          
+        ),
+    );
     
   }
-}
+  
+  withOpacity(double d) {}
+
