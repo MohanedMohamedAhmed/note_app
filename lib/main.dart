55d2bfb905088b20e6f6a69c5a9cefe0fd51c0c0
MohanedMohamedAhmed <132159595+MohanedMohamedAhmed@users.noreply.github.com>
2023-05-14 18:32:01 +0200

    register adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/note_model.dart';
 
 void main()  async{
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NoteMOdelAdapter());
   runApp(const NoteApp());
 }
 class NoteApp extends StatelessWidget {
